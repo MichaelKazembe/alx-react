f36e6a74b647d5a491987d57f2ad8009f71ebe5a
Michaelkazembe <Michaelkazembe@gmail.com>
2024-01-03 01:44:34 +0200

    refactored code

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,11 +1,5 @@
-import $ from 'jquery';
+import $  from "jquery";
 
-$(document).ready(function() {
-    // Create three different paragraphs
-    var paragraph1 = $('<p>').text('Holberton Dashboard');
-    var paragraph2 = $('<p>').text('Dashboard data for the students');
-    var paragraph3 = $('<p>').text('Copyright - Holberton School');
-
-    // Append the paragraphs to the page body
-    $('body').append(paragraph1, paragraph2, paragraph3);
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');

2b85bcb242322661caec6bda5694714d68e0e6dc
Michaelkazembe <Michaelkazembe@gmail.com>
2024-01-03 20:34:41 +0200

    updated

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,30 +1,38 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        main: path.resolve(__dirname, './js/dashboard_main.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000,
-    },
-    module: {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        test: /\.(gif|png|jp?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
-      // Add more rules for other file types if needed
     ],
   },
 };

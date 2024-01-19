87a7270d007947daa911f96f381f93d093f0a99a
Michaelkazembe <Michaelkazembe@gmail.com>
2024-01-19 21:29:03 +0200

    tests

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,51 +1,25 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow, mount } from "enzyme";
+import App from "./App";
 
-describe('Test App.js', () => {
-  let wrapper;
+describe("App tests", () => {
+  it("pressing ctrl+h triggers logOut function and alert", () => {
+    const logOutMock = jest.fn();
+    const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
 
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
+    const component = mount(<App isLoggedIn={true} logOut={logOutMock} />);
 
-  it('Renders App without crashing', () => {
-    expect(wrapper.exists());
-  });
+    const event = new KeyboardEvent("keydown", {
+      ctrlKey: true,
+      key: "h",
+    });
 
-  it('App component contains Notifications component', () => {
-    expect(wrapper.find("Notifications")).toHaveLength(1);
-  });
+    document.dispatchEvent(event);
 
-  it('App component contains Header component', () => {
-    expect(wrapper.find("Header")).toHaveLength(1);
-  });
+    expect(alertMock).toHaveBeenCalledWith("Logging you out");
+    expect(logOutMock).toHaveBeenCalled();
 
-  it('App component contains Login component', () => {
-    expect(wrapper.find("Login")).toHaveLength(1);
-  });
-
-  it('App component contains Footer component', () => {
-    expect(wrapper.find("Footer")).toHaveLength(1);
-  });
-
-  it('test to check that CourseList is not displayed inside App', () => {
-    expect(wrapper.find("CourseList")).toHaveLength(0);
-  });
-});
-
-describe("Testing <App isLoggedIn={true} />", () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(<App isLoggedIn={true}/>);
-  });
-
-  it("the Login component is not included", () => {
-    expect(wrapper.find('Login')).toHaveLength(0);
-  });
-
-  it(" the CourseList component is included", () => {
-    expect(wrapper.find('CourseList').exists());
+    component.unmount();
+    alertMock.mockRestore();
   });
 });

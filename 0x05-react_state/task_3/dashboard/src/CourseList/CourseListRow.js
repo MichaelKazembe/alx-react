46139b70e73592bc7ddb1988257c6890caaa192c
Michaelkazembe <Michaelkazembe@gmail.com>
2024-01-27 00:07:36 +0200

    Checkbox using react hook

--- 0x05-react_state/task_3/dashboard/src/CourseList/CourseListRow.js
+++ 0x05-react_state/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -1,30 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const [checked, setChecked] = useState(false);
   const headerStyle = {backgroundColor: '#deb5b545'};
   const rowStyle = {backgroundColor: '#f5f5f5ab'};
   const selected_style = isHeader ? headerStyle : rowStyle;
 
+  const handleCheckChange = (e) => {
+    setChecked(!checked);
+  };
+
   return (
-    <tr style={selected_style}>
+    <tr style={selected_style} className={checked ? css(rowsStyles.rowChecked) : ''}>
       {isHeader ?
         textSecondCell === null ?
-          <th colSpan="2">{textFirstCell}</th>
+          <th colSpan="2" className={css(rowsStyles.thcenter)}>{textFirstCell}</th>
         :
           <>
-            <th>{textFirstCell}</th>
-            <th>{textSecondCell}</th>
+            <th className={css(rowsStyles.th)}>{textFirstCell}</th>
+            <th className={css(rowsStyles.th)}>{textSecondCell}</th>
           </>
       :
         <>
-          <td>{textFirstCell}</td>
-          <td>{textSecondCell}</td>
+          <td className={css(rowsStyles.td)}>
+            <input type="checkbox" onChange={handleCheckChange}/>
+            {textFirstCell}</td>
+          <td className={css(rowsStyles.td)}>{textSecondCell}</td>
         </>
       }
     </tr>
   );
 }
+
+const rowsStyles = StyleSheet.create({
+  thcenter: {
+    borderBottom: '1px solid gray',
+    margin: 0,
+    padding: 0,
+    textAlign: 'center'
+  },
+  th: {
+    borderBottom: '1px solid gray',
+    margin: 0,
+    padding: 0,
+    textAlign: 'left'
+  },
+  td: {
+    paddingLeft: 3
+  },
+  rowChecked: {
+    backgroundColor: '#e6e4e4'
+  }
+});
 
 CourseListRow.defaultProps = {
   isHeader: false,
